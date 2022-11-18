983d9c14bef86f12583094660a7ec16d12c7b5d2
Ivan Filip Terzic <i.terzic@gmx.net>
2022-11-19 00:07:50 +0100

    add sql.ConnectionPool to express app & try connecting to databse on api startup

--- application/api/src/index.js
+++ application/api/src/index.js
@@ -1,13 +1,29 @@
 /* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+const logger = require("./logger");
+const app = require("./app");
+const sql = require("mssql");
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+const { database: dbConfig } = require("../config/dbConfig.json");
+const appPool = new sql.ConnectionPool(dbConfig);
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+appPool
+  .connect()
+  .then((pool) => {
+    app.locals.db = pool;
+    const port = app.get("port");
+    const server = app.listen(port);
+    server.on("listening", () =>
+      logger.info(
+        "Feathers application started on http://%s:%d",
+        app.get("host"),
+        port
+      )
+    );
+  })
+  .catch((e) => {
+    logger.error("Error creating connection pool", e);
+  });
+
+process.on("unhandledRejection", (reason, p) =>
+  logger.error("Unhandled Rejection at: Promise ", p, reason)
 );
