edc5d9b5dc0e1b1cd65d15010857f395928212ca
Ivan Filip Terzic <i.terzic@gmx.net>
2022-11-25 13:04:35 +0100

    some styling improvements

--- application/frontend/src/App.vue
+++ application/frontend/src/App.vue
@@ -3,11 +3,14 @@
     <v-navigation-drawer rail expand-on-hover color="#27292F" theme="dark">
       <!-- TODO: add navigation items here -->
       <v-list nav>
-        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="Master User"
-          subtitle="Master.User@gmail.com"></v-list-item>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="Master User"
+          subtitle="Master.User@gmail.com"
+        />
       </v-list>
 
-      <v-divider></v-divider>
+      <v-divider />
 
       <v-list density="compact" nav>
         <v-list-item :to="{ name: 'home' }" prepend-icon="mdi-home">
@@ -23,22 +26,23 @@
           Manager
         </v-list-item>
       </v-list>
-
     </v-navigation-drawer>
 
-    <v-app-bar color="white" elevation="1">
+    <v-app-bar color="grey-lighten-2" elevation="1">
       <v-toolbar-title>
         <!-- TODO: Application Name -->
         Property Management
       </v-toolbar-title>
-
     </v-app-bar>
 
     <v-main>
-      <v-sheet :color="currentRoute.meta.backgroundColor" style="height: calc(100vh - 64px)">
+      <v-sheet
+        :color="currentRoute.meta.backgroundColor"
+        style="height: calc(100vh - 64px)"
+      >
         <v-container>
           <!-- TODO: Add router-views -->
-          <router-view></router-view>
+          <router-view />
         </v-container>
       </v-sheet>
     </v-main>
@@ -52,13 +56,13 @@ export default {
   setup() {
     const drawer = ref(false);
 
-    const router = useRouter()
+    const router = useRouter();
     const { currentRoute } = router;
 
     return {
       drawer,
-      currentRoute
-    }
-  }
-}
+      currentRoute,
+    };
+  },
+};
 </script>
