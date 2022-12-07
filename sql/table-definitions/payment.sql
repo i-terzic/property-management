10563cd2b1cb18ab4b2ee3f68b61e19ac3ff94ba
Ivan Filip Terzic <i.terzic@gmx.net>
2022-12-07 13:27:43 +0100

    fix typo

--- sql/table-definitions/payment.sql
+++ sql/table-definitions/payment.sql
@@ -7,7 +7,7 @@ EXEC ('CREATE SCHEMA [group7]');
 GO
 
 -- ************************************** [Payment]
-IF NOT EXISTS (SELECT * FROM sys.tables t WHERE t.name='Property' AND t.schema_id IN ( SELECT schema_id  FROM sys.schemas s WHERE s.name='group7') )
+IF NOT EXISTS (SELECT * FROM sys.tables t WHERE t.name='Payment' AND t.schema_id IN ( SELECT schema_id  FROM sys.schemas s WHERE s.name='group7') )
 CREATE TABLE [group7].[Payment]
 (
  [paymentID] int NOT NULL ,
--- sql/table-definitions/unit.sql
+++ sql/table-definitions/unit.sql
@@ -7,7 +7,7 @@ EXEC ('CREATE SCHEMA [group7]');
 GO
 
 -- ************************************** [Unit]
-IF NOT EXISTS (SELECT * FROM sys.tables t WHERE t.name='Property' AND t.schema_id IN ( SELECT schema_id  FROM sys.schemas s WHERE s.name='group7') )
+IF NOT EXISTS (SELECT * FROM sys.tables t WHERE t.name='Unit' AND t.schema_id IN ( SELECT schema_id  FROM sys.schemas s WHERE s.name='group7') )
 CREATE TABLE [group7].[Unit]
 (
  [unitID]       int NOT NULL ,
