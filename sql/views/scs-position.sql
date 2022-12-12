584fe1a60410cc508be0abd364efb5507032d489
Ivan Filip Terzic <i.terzic@gmx.net>
2022-12-12 20:26:23 +0100

    add allocationTypeName to scs-position view

--- sql/views/scs-position.sql
+++ sql/views/scs-position.sql
@@ -2,13 +2,19 @@ CREATE VIEW [group7].[scs_position_view]
 AS
 SELECT
 	sp.scsPositionID,
-    sp.amount,
-    sp.positionID,
-    sp.date,
-    sp.tenantID,
-    p.positionName
+	sp.amount,
+	sp.positionID,
+	sp.date,
+	sp.tenantID,
+	p.positionName,
+	a.allocationTypeName
 FROM
 	[group7].[scsPosition] sp
 INNER JOIN 
-    [group7].[Position] p
-ON p.positionID = sp.positionID;
+    [group7].[Position] p	
+ON
+	p.positionID = sp.positionID
+INNER JOIN 
+	[group7].allocationType a
+ON
+	a.allocationTypeID = p.allocationTypeID;
